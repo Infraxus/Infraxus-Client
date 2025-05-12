ba9b9d912ceff2e335a6a3408ae320cabbb4f4ff
이재현 <ij184399@gmail.com>
2025-05-12 19:10:20 +0900

    feat(global): color setting

---
+++ src/shared/styles/color.ts
@@ -0,0 +1,25 @@
+const color = {
+    PrimaryColor: '#CB3CFF',
+
+    SecondaryColor1: '#0B1739',
+    SecondaryColor2: '#8951FF',
+    SecondaryColor3: '#21C3FC',
+    SecondaryColor4: '#0E43FB',
+    SecondaryColor5: '#FDB52A',
+
+    NeutralColor800: '#081028',
+    NeutralColor700: '#0A1330',
+    NeutralColor600: '#0B1739',
+    NeutralColor500: '#7E89AC',
+    NeutralColor400: '#AEB9E1',
+    NeutralColor300: '#D1DBF9',
+    NeutralColor200: '#D9E1FA',
+    NeutralColor100: '#FFFFFF',
+
+    Blue: '#4318D1',
+    Green: '#008000',
+    Red: '#DC2626',
+    Gray: '#808080',
+}
+
+export default color
