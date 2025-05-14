81c92546a82a134ba4e7976194d29d4c6bcbfb1e
이재현 <ij184399@gmail.com>
2025-05-15 00:35:46 +0900

    feat(new server): 새로운 서버 메인 화면 퍼블리싱 완료

---
+++ src/pages/new-server/index.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import color from '@/shared/styles/color.ts'
+import { Sidebar } from '@/shared/components/sidebar/sidebar';
+import { NewServerInput } from './ui/newServer';
+
+const PageContainer = styled.div`
+    display: flex;
+    background: ${color.NeutralColor800};
+`;
+
+const MainContainer = styled.div`
+    padding: 2.5vh 2.25vw;
+    width: 80vw;
+    height: 100vh;
+    flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 0.94rem
+`;
+
+const PageTitle = styled.p`
+    color: ${color.NeutralColor100};
+    font-family: "Inter";
+    font-size: 1.75rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 2.25rem;
+`;
+
+export const NewServer: React.FC = () => {
+
+    const architectures = [
+        {architectureName: "모놀리식 아키텍처", architectureDescription: "애플리케이션의 모든 기능이 하나의 단일 코드베이스로 통합되어, 전체 시스템이 하나의 단위로 개발되고 배포되는 구조입니다. 모놀리식 아키텍처와 동일하게 배포가 가능한 아키텍처 : 계층형, 헥사고날, 클린, 마이크로커널, 파이프&필터"},
+        {architectureName: "마이크로서비스 아키텍처", architectureDescription: "애플리케이션을 독립적으로 배포 가능한 작은 서비스들로 분리하여, 각 서비스가 독립적으로 개발, 배포, 확장 가능한 구조입니다."},
+        {architectureName: "커스텀 아키텍처", architectureDescription: "아키텍처를 마음대로 구축할 수 있습니다. 모든 기능을 제공합니다."},
+    ];
+
+    return (
+        <PageContainer>
+            <Sidebar />
+            <MainContainer>
+                <PageTitle>새로운 서버</PageTitle>
+                <NewServerInput architectures={architectures} />
+            </MainContainer>
+        </PageContainer>
+    );
+}
---
+++ src/pages/new-server/ui/newServer.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { styled } from 'styled-components';
+import color from '@/shared/styles/color.ts'
+import { useNavigate } from 'react-router-dom';
+
+const NewServerContainer = styled.div`
+    padding: 2.5vh 1.25vw;
+    width: 76.25vw;
+    height: 55vh;
+    flex-shrink: 0;
+    border-radius: 0.5rem;
+    background: ${color.SecondaryColor1};
+    display: flex;
+    flex-direction: column;
+    gap: 2vh;
+`;
+
+const ServerNameInput = styled.input`
+    width: 74.25vw;
+    height: 5vh;
+    flex-shrink: 0;
+    border-radius: 0.25rem;
+    background: ${color.NeutralColor800};
+    color: ${color.NeutralColor100};
+    font-size: 1rem;
+    padding: 0.5rem 1rem;
+    outline: none;
+    border: none;
+    box-shadow: none;
+    
+    ::placeholder {
+        color: ${color.NeutralColor400};
+        font-size: 1rem;
+    }
+`;
+
+const ArchitectureInputContainer = styled.div<ArchitectureInputProps>`
+    padding: 2vh 1.5vw;
+    width: 73vw;
+    min-height: 6vh;
+    flex-shrink: 0;
+    border-radius: 0.25rem;
+    background: ${({ $isSelected }) => $isSelected ? "var(--primary-color-linear, linear-gradient(128deg, #CB3CFF 19.86%, #7F25FB 68.34%))" : color.NeutralColor700};
+    display: flex;
+    flex-direction: column;
+    gap: 1vh;
+`;
+
+const ArchitectureInputTitle = styled.p`
+    color: ${color.NeutralColor100};
+    font-family: Inter;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.5rem; /* 150% */
+`;
+
+const ArchitectureInputDescription = styled.p`
+    width: 73.75vw;
+    min-height: 1vh;
+    color: ${color.NeutralColor400};
+    font-family: Inter;
+    font-size: 0.875rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.3125rem; /* 150% */
+`;
+
+const NextSubmitButton = styled.button`
+    width: 5vw;
+    height: 5vh;
+    padding: 1vh 0;
+    flex-shrink: 0;
+    border-radius: 0.25rem;
+    background: ${color.PrimaryColor};
+    outline: none;
+    border: none;
+    box-shadow: none;
+
+    color: ${color.NeutralColor100};
+    text-align: center;
+    font-family: Inter;
+    font-size: 1rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 1.3125rem; /* 150% */
+`;
+
+interface ArchitectureInputProps {
+    $isSelected: boolean;  // $로 시작하는 props는 styled-components에서 필터링됨
+}
+
+interface Architecture {
+    architectureName: string;
+    architectureDescription: string;
+}
+
+interface ArchitectureProps {
+    architectures: Architecture[];
+}
+
+export const NewServerInput: React.FC<ArchitectureProps> = ({ architectures }) => {
+    const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+    const [inputValue, setInputValue] = useState("");
+    
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleClick = (index: number) => {
+        setSelectedIndex(index === selectedIndex ? null : index);
+    };
+
+    const navigate = useNavigate();
+
+    return (
+        <NewServerContainer>
+            <ServerNameInput placeholder='서버 이름을 입력해주세요' value={inputValue} onChange={handleChange} />
+            {architectures.map((item, index) => {
+                return (
+                    <ArchitectureInputContainer
+                            key={index}
+                            $isSelected={index === selectedIndex}
+                            onClick={() => handleClick(index)}
+                        >
+                            <ArchitectureInputTitle>{item.architectureName}</ArchitectureInputTitle>
+                            <ArchitectureInputDescription>{item.architectureDescription}</ArchitectureInputDescription>
+                        </ArchitectureInputContainer>
+                )
+            })}
+            <NextSubmitButton onClick={() => navigate(`/new/config?name=${inputValue}&architecture${architectures[selectedIndex].architectureName}`)}>
+                생성
+            </NextSubmitButton>
+        </NewServerContainer>
+    );
+}
