114390b78d524e6ff5056dafeb613fd1167dd3e8
이재현 <ij184399@gmail.com>
2025-05-14 16:04:28 +0900

    feat(App.tsx): 각 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Sidebar } from "./shared/components/sidebar/sidebar.tsx";
+import { Route, Router, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard/index.tsx";
 
 const App = () => (
-  <Sidebar />
+  <Routes>
+    {/* <Route path="/new" element={<NewServer />} /> */}
+    <Route path="/" element={<Dashboard />} />
+    {/* <Route path="/server" element={<Server />} /> */}
+    {/* <Route path="/alram" element={<Alram />} /> */}
+    {/* <Route path="/setting" element={<Setting />} /> */}
+  </Routes>
 );
 
 export default App;
