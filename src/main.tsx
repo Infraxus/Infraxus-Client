020501d31b85a2639a3b2d3a728b32d2c5c7fd17
이재현 <ij184399@gmail.com>
2025-05-14 16:03:44 +0900

    feat(main): main.tsx Router add

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import App from './App.tsx';
+import './main.css';
+import { BrowserRouter, Router } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement,
+);
+  
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
