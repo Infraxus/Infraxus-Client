5b2f5e757725517aff1a466c0dd9b8d58779b62f
이재현 <ij184399@gmail.com>
2025-05-14 16:05:08 +0900

    feat(component): sidebar 퍼블리싱 완료

--- src/shared/components/sidebar/sidebar.tsx
+++ src/shared/components/sidebar/sidebar.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import color from '@/shared/styles/color.ts'
+//nomarl
+import nomarlBell from '@/assets/nomarl-bell-icon.svg';
+import nomarlBrowser from '@/assets/nomarl-browser-icon.svg';
+import nomarlPlus from '@/assets/nomarl-plus-icon.svg';
+import nomarlServer from '@/assets/nomarl-server-icon.svg';
+import nomarlSetting from '@/assets/nomarl-settings-icon.svg';
+// active
+import activeBell from '@/assets/active-bell-icon.svg';
+import activeBrowser from '@/assets/active-browser-icon.svg';
+import activePlus from '@/assets/active-plus-icon.svg';
+import activeServer from '@/assets/active-server-icon.svg';
+import activeSetting from '@/assets/active-settings-icon.svg';
 
 const SidebarContainer = styled.aside`
+  hegiht: 100vh;
+  width: 18.75rem;
+
   display: flex;
   flex-direction: column;
   align-items: stretch;
@@ -16,9 +32,8 @@ const SidebarContainer = styled.aside`
   box-shadow: 0.3125rem 0 0.25rem 0 rgba(0, 0, 0, 0.5);
   border-radius: 0 0.9375rem 0.9375rem 0;
   padding: 1.875rem 1.75rem 46.25rem;
-  margin: auto;
 
-  @media (max-width: 48rem) {
+  @media (max-width: 18.75rem) {
     margin-top: 1.875rem;
     padding-bottom: 6.25rem;
     padding-left: 1.25rem;
@@ -28,16 +43,21 @@ const SidebarContainer = styled.aside`
 
 const Logo = styled.div`
   color: ${color.NeutralColor100};
-  font-size: 2.5rem;
-  line-height: 1;
-  margin-bottom: 1.875rem;
+  text-align: center;
+  font-family: 'Anta';
+  font-weight: 400;
+  font-size: 2.25rem;
+  font-style: normal;
+  line-height: 2.5rem;
 `;
 
 const Nav = styled.nav`
   margin-top: 1.875rem;
 `;
 
-const MenuItem = styled.div<{ active: boolean }>`
+const MenuItem = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== 'active',
+})<{ active: boolean }>`
   display: flex;
   align-items: center;
   gap: 1rem;
@@ -62,24 +82,30 @@ const MenuItemLabel = styled.div`
 `;
 
 const menuItems = [
-  { icon: 'https://cdn.builder.io/api/v1/image/assets/c8df5d00d5254ba7af7ef0a63b65be18/dc5fcbdc00e6040d720d041e95fd491365aedb4c?placeholderIfAbsent=true', label: '새로운 서버', active: false },
-  { icon: 'https://cdn.builder.io/api/v1/image/assets/c8df5d00d5254ba7af7ef0a63b65be18/de4999bbe3dbabeff83583e893ef61404c512807?placeholderIfAbsent=true', label: '어드민 대시보드', active: true },
-  { icon: 'https://cdn.builder.io/api/v1/image/assets/c8df5d00d5254ba7af7ef0a63b65be18/f7bf89d5a245b6073f2740ab09bf6de2d2b389e5?placeholderIfAbsent=true', label: '서버 목록', active: false },
-  { icon: 'https://cdn.builder.io/api/v1/image/assets/c8df5d00d5254ba7af7ef0a63b65be18/3e4c0c74a73bdc3c69740661c5d84ad110ddd988?placeholderIfAbsent=true', label: '알림', active: false },
-  { icon: 'https://cdn.builder.io/api/v1/image/assets/c8df5d00d5254ba7af7ef0a63b65be18/5f741bae96c5657047d21542e16ec7ef580b42f4?placeholderIfAbsent=true', label: '설정', active: false },
+  { nomarlIcon: `${nomarlPlus}`, activeIcon: `${activePlus}`, label: '새로운 서버', path: "/new" },
+  { nomarlIcon: `${nomarlBrowser}`, activeIcon: `${activeBrowser}`, label: '어드민 대시보드', path: "/" },
+  { nomarlIcon: `${nomarlServer}`, activeIcon: `${activeServer}`, label: '서버 목록', path: "/server" },
+  { nomarlIcon: `${nomarlBell}`, activeIcon: `${activeBell}`, label: '알림', path: "/alram" },
+  { nomarlIcon: `${nomarlSetting}`, activeIcon: `${activeSetting}`, label: '설정', path: "/setting" },
 ];
 
 export const Sidebar: React.FC = () => {
+
+  
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <SidebarContainer>
       <Logo>Infraxus</Logo>
       <Nav>
-        {menuItems.map((item, index) => (
-          <MenuItem key={index} active={item.active}>
-            <MenuItemIcon src={item.icon} alt="" />
-            <MenuItemLabel>{item.label}</MenuItemLabel>
-          </MenuItem>
-        ))}
+        {menuItems.map((item, index) => {
+          return (
+            <MenuItem key={index} active={location.pathname === item.path} onClick={() => navigate(item.path)}>
+              <MenuItemIcon src={location.pathname === item.path ? item.activeIcon : item.nomarlIcon} alt="" />
+              <MenuItemLabel>{item.label}</MenuItemLabel>
+            </MenuItem>
+        )})}
       </Nav>
     </SidebarContainer>
   );
