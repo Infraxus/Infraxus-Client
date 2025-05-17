aaf0f618dc4439f01afe556ca1b67a6664faf186
ljhy07 <ij184399@gmail.com>
2025-05-17 16:01:36 +0900

    feat(App): 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Route, Router, Routes } from "react-router-dom";
 import { Dashboard } from "./pages/dashboard/index.tsx";
 import { NewServer } from "./pages/new-server/index.tsx";
 import { NewServerConfig } from "./pages/new-server/ui/config/index.tsx";
+import { NewServerContainer } from "./pages/new-server/ui/container/index.tsx";
 
 const App = () => (
   <Routes>
     <Route path="/new" element={<NewServer />} />
     <Route path="/new/config" element={<NewServerConfig />} />
+    <Route path="/new/container" element={<NewServerContainer />} />
     <Route path="/" element={<Dashboard />} />
     {/* <Route path="/server" element={<Server />} /> */}
     {/* <Route path="/alram" element={<Alram />} /> */}
