2c6f360837b077de05bf66db1a155de0b0f81af4
ljhy07 <ij184399@gmail.com>
2025-05-18 18:03:48 +0900

    feat(new server): 컨테이너 폼 요소 크기 수정

--- src/shared/components/new-server/formInput.tsx
+++ src/shared/components/new-server/formInput.tsx
@@ -13,20 +13,20 @@ const Container = styled.div`
 
 const Label = styled.label`
   color: ${color.NeutralColor100};
-  font-size: 0.9375vh;  /* 약 12px */
+  font-size: 1rem;  /* 약 12px */
   margin: 0.5vh 0;  /* 약 2px */
   display: block;
 `;
 
 const StyledInput = styled.input`
-  width: 97.65%;
-  height: 5vh;  /* 약 50px */
+  width: 83.75vw;
+  height: 4vh;  /* 약 50px */
   background-color: ${color.NeutralColor600};
   border-radius: 0.26vh;  /* 약 4px */
   border: none;
-  padding: 0 1vw;  /* 약 15px */
+  padding: 0 0.5vw;  /* 약 15px */
   color: ${color.NeutralColor100};
-  font-size: 1rem;  /* 약 14px */
+  font-size: 0.75rem;  /* 약 14px */
 `;
 
 export const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(
