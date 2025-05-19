1423d532d5f0a82630d4fdeef4de11dd25d846e4
이재현 <ij184399@gmail.com>
2025-05-19 23:55:44 +0900

    feat(new server): 예시 데이터 수정

--- src/pages/new-server/index.tsx
+++ src/pages/new-server/index.tsx
@@ -31,9 +31,9 @@ const PageTitle = styled.p`
 export const NewServer: React.FC = () => {
 
     const architectures = [
-        {architectureName: "모놀리식 아키텍처", architectureDescription: "애플리케이션의 모든 기능이 하나의 단일 코드베이스로 통합되어, 전체 시스템이 하나의 단위로 개발되고 배포되는 구조입니다. 모놀리식 아키텍처와 동일하게 배포가 가능한 아키텍처 : 계층형, 헥사고날, 클린, 마이크로커널, 파이프&필터"},
-        {architectureName: "마이크로서비스 아키텍처", architectureDescription: "애플리케이션을 독립적으로 배포 가능한 작은 서비스들로 분리하여, 각 서비스가 독립적으로 개발, 배포, 확장 가능한 구조입니다."},
-        {architectureName: "커스텀 아키텍처", architectureDescription: "아키텍처를 마음대로 구축할 수 있습니다. 모든 기능을 제공합니다."},
+        {architecture: "monolithic", architectureName: "모놀리식 아키텍처", architectureDescription: "애플리케이션의 모든 기능이 하나의 단일 코드베이스로 통합되어, 전체 시스템이 하나의 단위로 개발되고 배포되는 구조입니다. 모놀리식 아키텍처와 동일하게 배포가 가능한 아키텍처 : 계층형, 헥사고날, 클린, 마이크로커널, 파이프&필터"},
+        {architecture: "microservices", architectureName: "마이크로서비스 아키텍처", architectureDescription: "애플리케이션을 독립적으로 배포 가능한 작은 서비스들로 분리하여, 각 서비스가 독립적으로 개발, 배포, 확장 가능한 구조입니다."},
+        // {architectureName: "커스텀 아키텍처", architectureDescription: "아키텍처를 마음대로 구축할 수 있습니다. 모든 기능을 제공합니다."},
     ];
 
     return (
