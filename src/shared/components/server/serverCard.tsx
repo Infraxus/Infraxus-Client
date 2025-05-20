df48153365f655fdd50a830db660de6b93dbcfac
이재현 <ij184399@gmail.com>
2025-05-20 23:59:36 +0900

    feat(server): 서버 메인 페이지 서버 카드 컴포넌트

---
+++ src/shared/components/server/serverCard.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import { StatusIndicator } from './statusIndicator';
+import { MetricCard } from './metricCard.tsx';
+import { useNavigate } from 'react-router-dom';
+
+interface ServerCardProps {
+  id: number
+  name: string;
+  status: 'running' | 'stopped' | 'error';
+  metrics: {
+    cpu: string;
+    memory: string;
+    created: string;
+    redistribution: string;
+  };
+}
+
+const CardContainer = styled.div`
+  background: #0B1739;
+  padding: 2.5vh 1.5vw;
+  border-radius: 0.75vw;
+  display: flex;
+  flex-direction: column;
+  gap: 2.5vh;
+`;
+
+const TopSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ServerName = styled.p`
+  flex: 1;
+  color: white;
+  font-size: 1.25rem;
+  justify-content: center;
+  align-items: center;
+  height: 3.5vh;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  gap: 0.57vw;
+  justify-content: flex-end;
+  flex: 1;
+`;
+
+const ActionButton = styled.button<{ $variant?: 'primary' | 'danger' | 'outline' }>`
+  border-radius: 0.5vw;
+  font-size: 1rem;
+  cursor: pointer;
+  padding: 1.2vh 1.75vw;
+  border: ${({ $variant }) =>
+    $variant === 'outline' ? '0.15vw solid #DC2626' : 'none'};
+  background: ${({ $variant }) =>
+    $variant === 'primary' ? '#0E43FB' :
+    $variant === 'danger' ? '#DC2626' :
+    'transparent'};
+  color: ${({ $variant }) =>
+    $variant === 'outline' ? '#DC2626' : '#FFFFFF'};
+`;
+
+const MetricsSection = styled.div`
+  display: flex;
+  gap: 1.1vw;
+  flex-wrap: wrap;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 2.5vh;
+  }
+`;
+
+export const ServerCard: React.FC<ServerCardProps> = ({ id, name, status, metrics }) => {
+  const isRunning = status === 'running';
+  const navigate = useNavigate();
+
+  const onHandleDetail = () => {
+    navigate(`/server/detail/${id}`);
+  }
+
+  return (
+    <CardContainer onClick={() => onHandleDetail}>
+      <TopSection>
+        <StatusIndicator status={status} />
+        <ServerName>{name}</ServerName>
+        <ButtonGroup>
+          <ActionButton $variant={isRunning ? 'danger' : 'primary'}>
+            {isRunning ? 'Stop' : 'Start'}
+          </ActionButton>
+          <ActionButton $variant="outline">Delete</ActionButton>
+        </ButtonGroup>
+      </TopSection>
+      <MetricsSection>
+        <MetricCard label="CPU Usage" value={metrics.cpu} />
+        <MetricCard label="Memory Usage" value={metrics.memory} />
+        <MetricCard label="Created" value={metrics.created} />
+        <MetricCard label="Redistribution" value={metrics.redistribution} />
+      </MetricsSection>
+    </CardContainer>
+  );
+};
