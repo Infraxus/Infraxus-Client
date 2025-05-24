bc5be32c0e2b4128c76ed5763d2629da94babfc7
이재현 <ij184399@gmail.com>
2025-05-24 19:54:09 +0900

    feat(server): 컨테이너 리스트의 테이블 헤더 컴포넌트 추가

---
+++ src/shared/components/server/detail/container-list/containerTableHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const ContainerTableHeader: React.FC = () => (
+  <tr>
+    <th>Name</th>
+    <th>IP/Port</th>
+    <th>Status</th>
+    <th>CPU (%)</th>
+    <th>Memory (GB)</th>
+    <th>Actions</th>
+  </tr>
+);
