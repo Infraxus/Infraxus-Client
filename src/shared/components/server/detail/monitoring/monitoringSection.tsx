e08f3bf090afc790532f9c6de80292bb75466843
이재현 <ij184399@gmail.com>
2025-05-25 20:14:23 +0900

    feat(server): 모니터링 컨테이너 상태 통합 컴포넌트 수정

--- src/shared/components/server/detail/monitoring/monitoringSection.tsx
+++ src/shared/components/server/detail/monitoring/monitoringSection.tsx
@@ -3,32 +3,24 @@ import styled from 'styled-components';
 import { ContainerStatusProps, AlertProps } from './types';
 
 const SectionWrapper = styled.div`
-  margin-top: 3.5vh;
   display: flex;
-  gap: 3.5vw;
+  gap: 1.5vw;
   flex-wrap: wrap;
 `;
 
-const HalfWidthWrapper = styled.div`
-  width: 47vw;
-  @media (max-width: 768px) {
-    width: 100%;
-    margin-left: 0;
-  }
-`;
-
 const Card = styled.div`
-  width: 100%;
+  width: 42.85vw;
   display: flex;
   flex-direction: column;
+  gap: 1.5vh;
   background-color: #0B1739;
-  padding: 4vh 3.5vw;
+  padding: 3vh 2vw;
   border-radius: 1vw;
 `;
 
 const Title = styled.h3`
   color: white;
-  font-size: 1.8vw;
+  font-size: 1.75rem;
   font-weight: 600;
 `;
 
@@ -37,7 +29,6 @@ const StatusItem = styled.div`
   justify-content: space-between;
   background-color: #081028;
   padding: 2vh 2.5vw;
-  margin-top: 2vh;
   border-radius: 0.8vw;
   flex-wrap: wrap;
   color: white;
@@ -106,12 +97,8 @@ export const MonitoringSection: React.FC = () => {
 
   return (
     <SectionWrapper>
-      <HalfWidthWrapper>
-        <ContainerStatus containers={containers} />
-      </HalfWidthWrapper>
-      <HalfWidthWrapper>
-        <ActiveAlerts alerts={alerts} />
-      </HalfWidthWrapper>
+      <ContainerStatus containers={containers} />
+      <ActiveAlerts alerts={alerts} />
     </SectionWrapper>
   );
 };
