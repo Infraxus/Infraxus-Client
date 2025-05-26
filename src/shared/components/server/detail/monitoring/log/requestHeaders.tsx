d663a5ada8ef057e3e25f681287da869ab5c44b8
이재현 <ij184399@gmail.com>
2025-05-26 20:18:21 +0900

    feat(server): 로그 요청 헤더 내용 컴포넌트 수정

--- src/shared/components/server/detail/monitoring/log/requestHeaders.tsx
+++ src/shared/components/server/detail/monitoring/log/requestHeaders.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface HeaderItem {
   key: string;
@@ -9,28 +10,80 @@ interface RequestHeadersProps {
   headers: HeaderItem[];
 }
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1vh;
+`;
+
+const Label = styled.div`
+  color: rgba(174, 185, 225, 1);
+  font-size: 1.25rem;
+  font-weight: 400;
+`;
+
+const HeaderBox = styled.div`
+  border-radius: 0.5vw;
+  background-color: rgba(10, 19, 48, 1);
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3vh 5vw;
+  font-size: 1rem;
+  font-weight: 400;
+  white-space: nowrap;
+  padding: 1.2vh 0.6vw;
+  width: 100%;
+`;
+
+const KeyColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5vh;
+  flex: 1;
+  color: rgba(174, 185, 225, 1);
+
+  & > div {
+
+
+    &:first-child {
+  
+    }
+  }
+`;
+
+const ValueColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5vh;
+  flex: 1;
+  color: white;
+
+  & > div {
+
+    padding-left: 1vw;
+
+    &:first-child {
+  
+    }
+  }
+`;
+
 export const RequestHeaders: React.FC<RequestHeadersProps> = ({ headers }) => {
   return (
-    <>
-      <div className="text-[rgba(174,185,225,1)] text-sm font-normal mt-6">
-        Request Headers
-      </div>
-      <div className="rounded bg-[rgba(10,19,48,1)] self-stretch flex items-stretch gap-[40px_100px] text-sm font-normal whitespace-nowrap flex-wrap mt-2 px-[11px] py-3 max-md:max-w-full">
-        <div className="flex flex-col text-[rgba(174,185,225,1)] flex-1">
+    <Container>
+      <Label>Request Headers</Label>
+      <HeaderBox>
+        <KeyColumn>
           {headers.map((header) => (
-            <div key={header.key} className="mt-2 first:mt-0">
-              {header.key}
-            </div>
+            <div key={header.key}>{header.key}</div>
           ))}
-        </div>
-        <div className="text-white flex-1">
+        </KeyColumn>
+        <ValueColumn>
           {headers.map((header) => (
-            <div key={`${header.key}-value`} className="mt-2 first:mt-0 pl-[19px]">
-              {header.value}
-            </div>
+            <div key={`${header.key}-value`}>{header.value}</div>
           ))}
-        </div>
-      </div>
-    </>
+        </ValueColumn>
+      </HeaderBox>
+    </Container>
   );
-};
+} 
