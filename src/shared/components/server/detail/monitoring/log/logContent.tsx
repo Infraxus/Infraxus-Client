10cafee25678ab1dfe97eb4e0b960c704552debb
이재현 <ij184399@gmail.com>
2025-05-26 20:17:46 +0900

    feat(server): 로그 상세 내용 컴포넌트 수정

--- src/shared/components/server/detail/monitoring/log/logContent.tsx
+++ src/shared/components/server/detail/monitoring/log/logContent.tsx
@@ -1,20 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface LogContentProps {
   title: string;
   content: string;
 }
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1vh;
+`;
+
+const Title = styled.h3`
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
+
+const Subtitle = styled.div`
+  color: rgba(174, 185, 225, 1);
+  font-size: 1.25rem;
+  font-weight: 400;
+`;
+
+const ContentBox = styled.div`
+  background-color: rgba(10, 19, 48, 1);
+  border-radius: 0.8vw;
+  color: white;
+  font-size: 1rem;
+  font-weight: 400;
+  padding: 2vh 1vw;
+  width: 95%;
+  word-break: break-word;
+`;
+
 export const LogContent: React.FC<LogContentProps> = ({ title, content }) => {
   return (
-    <>
-      <h3 className="text-white text-base font-medium mt-4">{title}</h3>
-      <div className="text-[rgba(174,185,225,1)] text-sm font-normal mt-2">
-        Log Content
-      </div>
-      <div className="rounded bg-[rgba(10,19,48,1)] self-stretch text-sm text-white font-normal mt-2 p-3 max-md:max-w-full max-md:pr-5">
-        {content}
-      </div>
-    </>
+    <Container>
+      <Title>{title}</Title>
+      <Subtitle>Log Content</Subtitle>
+      <ContentBox>{content}</ContentBox>
+    </Container>
   );
 };
